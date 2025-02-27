30577c412e61bf09dd16c0c963953814f671d36d
Sai-Ashritha <saiashrithaamaravathi@gmail.com>
2025-02-27 13:04:43 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -4,26 +4,29 @@ from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
 import os
+from langchain.schema.runnable import RunnablePassthrough
 
-# Set up Google GenAI API Key
-os.environ["GOOGLE_API_KEY"] = "your_google_api_key"
+from dotenv import load_dotenv
+load_dotenv()
 
-# Initialize Google GenAI Model
-llm = ChatGoogleGenerativeAI(model="gemini-pro")
+os.environ["GEMINI_API_KEY"] = os.getenv("GEMINI_API_KEY")
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash-exp")
 
-# Define prompt template for travel recommendations
+
 prompt_template = PromptTemplate(
-    input_variables=["source", "destination"],
     template="""
     You are an AI travel assistant. A user wants to travel from {source} to {destination}. 
     Provide travel options for cab, train, bus, and flight with estimated prices and travel times.
     """
 )
 
-# Create an LLM chain
-chain = LLMChain(llm=llm, prompt=prompt_template)
 
-# Streamlit UI
+
+chain = {"source": RunnablePassthrough(), 
+         "destination": RunnablePassthrough(), 
+         "preference": RunnablePassthrough()} | prompt_template | llm
+
+
 st.title("AI-Powered Travel planner")
 
 
@@ -33,7 +36,7 @@ destination = st.text_input("Enter Destination Location")
 
 if st.button("Find Travel Options"):
     if source and destination:
-        response = chain.run({"source":source,"destination":destination})
-        st.write(response)
+        response = chain.invoke({"source":source,"destination":destination})
+        st.write(response.content)
     else:
         st.warning("Please enter both source and destination")
